65728d92ef86a36f48e42fe46ab32b73808e94c0
Zech Drinkhall <zechdrink@aol.com>
2019-12-10 13:17:39 -0800

    minor updates

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -15,8 +15,8 @@ class Skills extends React.Component {
                 <p id = "skillsection-p"> These technologies are what I specialize in, but I am constantly trying to expand my scope of knowledge</p>
                 <div className = "skills-wrapper">
                     <SkillCard icon = {<i class="far fa-star"></i>} skill = {"Team Work"} description = {"One of my top priorities is to have healthy communication with teammates to maximize efficiency and code quality."}/>
-                    <SkillCard icon = {<i class="fab fa-react"></i>} skill = {"Frontend Development"} description = {"Pixel perfect front end development, using technologies like React, Redux, and SCSS."} />
-                    <SkillCard icon = {<i class="fas fa-sitemap"></i>} skill = {"Backend Development"} description = {"Robust test driven development with Ruby on Rails, NodeJS, Express and PostgreSQL."} />
+                    <SkillCard icon = {<i class="fab fa-react"></i>} skill = {"Frontend Development"} description = {"Building fast, responsive and dynamic applications using technologies like React, Redux and SCSS."} />
+                    <SkillCard icon = {<i class="fas fa-sitemap"></i>} skill = {"Backend Development"} description = {"Robust test driven development with technologies like Ruby on Rails, NodeJS, Express and PostgreSQL."} />
                 </div>
             </div>
          );
